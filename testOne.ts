a36296f45488915c0da73ed3799227c07672c48b
Ben Line <hello@benline.co.uk>
2021-07-23 18:47:15 +0100

    Update testOne.ts

--- testOne.ts
+++ testOne.ts
@@ -2,3 +2,8 @@ import { testTwo } from './testTwo.ts';
 import { testThree } from 'https://raw.githubusercontent.com/Benwebdev/deno/main/testThree.ts';
 
 export const testOne = () => console.log("deno works");
+
+export {
+  testTwo, 
+  testThree
+}
