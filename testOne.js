b033233cec32805f029c1fac500820730c781665
Ben Line <hello@benline.co.uk>
2021-07-23 18:45:02 +0100

    Update testOne.js

--- testOne.js
+++ testOne.js
@@ -1,1 +1,4 @@
+import { testTwo } from './testTwo.ts';
+import { testThree } from 'https://raw.githubusercontent.com/Benwebdev/deno/main/testThree.ts';
+
 export const testOne = () => console.log("deno works");
